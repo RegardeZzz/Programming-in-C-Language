a2081d0475e40c0e1d5cf8b08d5707c819fe8f45
Kirill <belkov-ki@mail.ru>
2026-02-04 17:48:00 +1000

    Сделал половину 2 Лабораторки на расчитывание 3x3 матрицы

--- Лабораторная 2/Lab2.c
+++ Лабораторная 2/Lab2.c
@@ -1,7 +1,26 @@
 #include<stdio.h>
-#include<math.h>
 
 int main()
 {
-    float a,b;
+    double matrix_3x3[3][3];
+    double MainDiagonal = 0;
+    double SideDiagonal = 0;
+
+    printf("Enter 9 numbers for 3x3 matrix:\n");
+
+    for(int row = 0; row < 3; row++) {
+        printf("Enter row %d:", row + 1);
+       for(int col = 0; col < 3; col++) {
+           scanf("%lf", &matrix_3x3[row][col]);
+       }
+    }
+
+    for(int row = 0; row < 3; row++) {
+        MainDiagonal = MainDiagonal + matrix_3x3[row][row];
+        SideDiagonal = SideDiagonal + matrix_3x3[row][2 - row];
+    }
+    printf("Main diagonal = %.2f\n", MainDiagonal);
+    printf("Side diagonal = %.2f\n", SideDiagonal);
+
+    return 0;
 }
